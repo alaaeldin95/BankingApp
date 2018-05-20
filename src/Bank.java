06bf9e115b046178c0750c7c27a7e5a2fe97565e
alaaeldin95 <37330751+alaaeldin95@users.noreply.github.com>
2018-05-20 03:26:47 +0200

    Version 1.2

--- src/Bank.java
+++ src/Bank.java
@@ -9,7 +9,10 @@ public class Bank {
         this.branches =  new ArrayList<Branch>();
     }
 
-
+    /**Finding branch by it's name
+     * @param nameOfBranch
+     * @return branch found
+     * @return null, if the branch doesn't exist*/
     private Branch findBranch(String nameOfBranch) {
         for (int i = 0; i < branches.size(); i++) {
             Branch branch = branches.get(i);
@@ -20,8 +23,10 @@ public class Bank {
         return null;
     }
 
-
-
+    /**Adding new branch to the bank passing the name of the branch
+     * @param branchName, the name going to be added
+     * @return true, if the name of branch is not exist yet in the branch ArrayList
+     * @return false, if the name of branch already exist in the arrayList*/
     public boolean addNewBranch(String branchName) {
         if (findBranch(branchName) == null) {
             this.branches.add(new Branch(branchName));
@@ -31,6 +36,12 @@ public class Bank {
         return false;
     }
 
+    /**Adding new Customer to the arrayList of Customers
+     * @param branchName, the name of the branch where we ar going to add the customer
+     * @param customerName, name of customer
+     * @param initialAmount, the initial amount to add to the new customer
+     * @return false if the branch doesn't exist, else
+     * @return the branch method that add a new customer to the arrayList of customers (this method is boolean too)*/
     public boolean addNewCustomer(String branchName, String customerName, double initialAmount) {
         Branch branch = findBranch(branchName);
         if (branch != null) {
@@ -40,6 +51,10 @@ public class Bank {
         return false;
     }
 
+    /**Adding customer transaction
+     * @param branchName,customerName,amount
+     * @return false if the branch doesn't exist
+     * @return the method that call addCustomerTransaction in branch class*/
     public boolean addCustomerTransaction(String branchName,String customerName, double amount) {
         Branch branch = findBranch(branchName);
         if (branch != null) {
@@ -49,6 +64,11 @@ public class Bank {
         return false;
     }
 
+    /**Print the list of customers in a particolar branch
+     * @param branchName the name of branch
+     * @param showTransactions, a boolean to choose to show the transactions of the customer or not
+     * @return true if everything gone well,
+     * @return false if the branch doesn't exist*/
     public boolean listCustomers(String branchName, boolean showTransactions) {
         Branch branch = findBranch(branchName);
         if (branch == null) {
